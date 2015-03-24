7f608de646b993b116172541e48ffcb7d2657f54
Cyberspace27 <ariel.villarreal26@gmail.com>
2015-03-23 23:06:54 -0600

    Se completa la creacion de las views sigin and  sigup y se modifica el controller user estas actualizaciones corresponden a los capitulos 71 y 72 del curso de angularJS

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,8 +1,102 @@
 //Invocar el modo JavaScript 'strict'
 'use strict';
 
-//cargar el modelo mongoose 'user'
-var User = require('mongoose').model('User');
+//cargar el modulo dependecies
+var User = require('mongoose').model('User'),
+passport = require('passport');
+//crear un nuevo metodo controller manejador de errores
+
+var getErrorMessage = function(err){
+	//Definir la variable de error message
+	var message = '';
+
+	//si un error interno de MongoDB ocurre obtener el mesaje de error
+	if(err.code){
+		switch(err.code){
+			// Si un error de index ocurre configurar el mensaje de error
+			case 11000:
+			case 11001:
+			message = 'Usuario ya existete';
+			break;
+			// Si un error de general ocurre configurar el mensaje de error
+			default:
+			message = 'se ha producido un error';
+		}
+	}else{
+		//Grabar el primer mensaje de error de una lista de posible errores
+		for (var errName in err.errors) {
+			if(err.errors[errName].message) message = err.errors[errName].message;
+
+		}
+	}
+	//Devolver el mensaje de error
+	return message;
+};
+//crear un nuevo metodo controller que renderiza la pagina signin
+exports.renderSignin = function(req, res, next){
+	// si el usuario no esta conectado renderizar la pagina signin, en otro caso redirecionar el usuario
+	if(!req.user){
+		// Usa el objeto 'response' para renderizar la pagia signin
+		res.render('signin', {
+			title: 'Sign-in Form',
+			//Configurar la variable del mensaje de flash
+			messages: req.flash('error') || req.flash('info')
+		});
+	} else{
+		return res.redirect('/');
+	}
+};
+//Crear un nuevo metodo controller que renderiza la pagina signup
+
+exports.renderSignup = function(req, res, next){
+	// si el usario no esta conectado renderizar la pagina signin, e otro caso redirecionar al usario
+	if(!req.user){
+		//Usa el objeto 'response' para renderzar la pagia signup
+		res.render('signup', {
+			title: 'Sign-up Form',
+			//Configura la variable flash
+			messages:req.flash('error')
+		});
+	}else{
+		return res.redirect('/');
+	}
+};
+
+//crear un nuevo metodo controller que crea nuevos users 'regular'
+exports.signup = function(req, res, next){
+	//Si user no esta conectado, crear y hacer login a un nuevo usuario, en otro caso redirecionar el user de vuelta a la pagina principal de la aplicaio
+	if (!req.user) {
+		//crear una nueva instancia del modelo 'User'
+		var user = new User(req.body);
+		var message = null;
+		//Configurar la propiedad user provider
+		user.provider = 'local';
+		//Intenta salvar el nuevo documento user
+		user.save(function(err){
+			//Si  ocurre un error, usa el mensaje flash para reportar el error
+			if(err){
+				//usa el metodo de mmanejo de errores para obtener el mensaje de error
+				var message = getErrorMessage(err);
+				//Configurar los mensajes flash
+				req.flash('error', message);
+				//Redirecciona al usuario de vuelta a la pagina signup
+				return res.redirect('/signup');
+			}
+			//Si el usuario fue creado de modo correcto usa el metodo 'login' de passport para hacer login
+			req.login(user, function(err){
+				//si ocurre un error de login moverse al siguiente middleware
+				if(err)return next(err);
+				//redirecionar el usuario de vuelta a la pagina de la aplicacion principal
+				return red.redirect('/')
+			});
+		});
+
+	}else{
+		return red.redirect('/');
+	}
+
+};
+
 
 //crear un nuevo metodo controller'create'
 exports.create = function(req, res, next){
@@ -90,7 +184,13 @@ exports.userByID = function(req, res, next, id){
 	
 };
 
-
+//crear un nuevo metodo controller para signing out
+exports.signout = function(req, res){
+	//Usa el metodo 'logout' de passport para hacer logout
+	req.logout();
+	//Rendirecciona al usuario de vuelta a la pagina de la aplicacion principal
+	res.redirect('/')
+};
 
 
 
